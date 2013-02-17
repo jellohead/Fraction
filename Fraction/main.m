bdccd55d3716aa75b011f77182908f8df4c5b625
Pat Escalona <jellohead7000@gmail.com>
2013-02-17 08:43:30 -0800

    Initial Commit

---
+++ Fraction/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by Pat on 2/17/13.
+//  Copyright (c) 2013 Pat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
