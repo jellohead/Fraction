742bf9554f5d00d4d55383a8ade3341437f144f0
Pat Escalona <jellohead7000@gmail.com>
2013-02-25 20:08:18 -0800

    Committing changes to make program functional

--- Fraction/main.m
+++ Fraction/main.m
@@ -8,14 +8,73 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+{
+    int numerator;
+    int denominator;
+}
+
+- (void) print;
+- (void) setNumerator:(int) n;
+- (void) setDenominator:(int)d;
+- (int) numerator;
+- (int) denominator;
+- (double) convertToNum;
+
+@end
+
+@implementation Fraction
+
+- (void) print
+{
+    NSLog(@" %i/%i ", numerator, denominator);
+}
+- (void) setNumerator:(int) n
+{
+    numerator = n;
+}
+- (void) setDenominator:(int)d
+{
+    denominator = d;
+}
+- (int) numerator
+{
+    return numerator;
+}
+- (int) denominator
+{
+    return denominator;
+}
+- (double) convertToNum
+{
+    if (denominator !=0)
+        return (double) numerator/denominator;
+    else
+        return 0.0;
+}
+
+@end
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *aFraction = [[Fraction alloc]init];
+        Fraction *bFraction = [[Fraction alloc]init];
         
+        [aFraction setNumerator:1];
+        [aFraction setDenominator:4];
+        
+        [aFraction print];
+        NSLog(@" =");
+        NSLog(@"%g",[aFraction convertToNum]);
+        
+        [bFraction print];
+        NSLog(@" =");
+        NSLog(@"%g", [bFraction convertToNum]);
+        
+              
     }
     return 0;
 }
